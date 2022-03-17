a2ae2556a938c1bfe74c4fe96f5d15b92105e319
Pedro Santos <pedrohrsantos01@gmail.com>
2022-03-17 17:48:25 -0300

    Welcome Screen done

--- screens/AppSwitchNavigator/WelcomeScreen.js
+++ screens/AppSwitchNavigator/WelcomeScreen.js
@@ -1,11 +1,66 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 
+import { Ionicons } from "@expo/vector-icons";
+import colors from "../../src/assets/colors";
+import CustomActionbutton from "../../src/components/CustomActionbutton";
 export default class WelcomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Welcome Screen</Text>
+      <View style={{ flex: 1, backgroundColor: colors.bgMain }}>
+        <View
+          style={{
+            flex: 1,
+            borderColor: "blue",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Ionicons name="ios-bookmarks" size={150} color={colors.logoColor} />
+          <Text
+            style={{
+              fontSize: 50,
+              fontWeight: "100",
+              color: "white",
+            }}
+          >
+            Book Worm
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            borderColor: "orange",
+            alignItems: "center",
+            justifyContent: "flex-start",
+          }}
+        >
+          <CustomActionbutton
+            style={{
+              width: 200,
+              backgroundColor: "transnparent",
+              borderWidth: 2,
+              borderColor: colors.bgPrimary,
+              marginBottom: 10,
+            }}
+            title="Login in"
+            onPress={() => alert("Login")}
+          >
+            <Text style={{ fontWeight: "100", color: "white" }}>Login in</Text>
+          </CustomActionbutton>
+          <CustomActionbutton
+            style={{
+              width: 200,
+              backgroundColor: "transnparent",
+              borderWidth: 2,
+              borderColor: colors.bgError,
+            }}
+            title="Sign up"
+            onPress={() => alert("Sign up")}
+          >
+            <Text style={{ fontWeight: "100", color: "white" }}>Sign up</Text>
+          </CustomActionbutton>
+        </View>
       </View>
     );
   }
--- src/assets/colors.js
+++ src/assets/colors.js
@@ -2,10 +2,12 @@ const colors = {
   bgSucess: "#a5deba",
   bgError: "#deada5",
   bgPrimary: "#aad1e6",
+  bgMain: "#2e424d",
   bgTextInput: "#ececec",
   bgColor: "#f7f2f2",
   textPlaceholder: "grey",
   borderColor: "#e9e9e9",
+  logoColor: "#bada55",
 };
 
 export default colors;
