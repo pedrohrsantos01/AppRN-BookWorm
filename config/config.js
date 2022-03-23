b903626fbc660b0eefc82f12a616d62702e96be7
Pedro Santos <pedrohrsantos01@gmail.com>
2022-03-23 17:09:49 -0300

    Setting Up Firebase

---
+++ config/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAbm_Cn6uQ-aS0T3cld0Yss1UtLk1Qq5_A",
+  authDomain: "book-worm-ab7ff.firebaseapp.com",
+  projectId: "book-worm-ab7ff",
+  storageBucket: "book-worm-ab7ff.appspot.com",
+  messagingSenderId: "534788848157",
+  appId: "1:534788848157:web:068b8041ff1f1d3dc064f4",
+  measurementId: "G-FQ75S0Y8W5",
+};
